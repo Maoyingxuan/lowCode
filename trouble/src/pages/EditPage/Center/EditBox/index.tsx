d60dcc6f2f96402280f919dc1780a886b50c40fc
Maoyingxuan <1584953989@qq.com>
2024-07-25 20:19:01 +1000

    增加组件旋转功能

--- trouble/src/pages/EditPage/Center/EditBox/index.tsx
+++ trouble/src/pages/EditPage/Center/EditBox/index.tsx
@@ -12,6 +12,7 @@ import {useEffect,useState} from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import Menu from "../Menu";
 import AlignLines from "./AlignLines";
+import Rotate from "./Rotate";
 export default function EditBox() {
   const zoom = useZoomStore((state) => state.zoom);
   const [canvas, assembly] = useEditStore((state) => [
@@ -32,6 +33,9 @@ export default function EditBox() {
     const [showMenu, setShowMenu] = useState(false);
 
   const onMouseDownOfCmp =(e:React.MouseEvent<HTMLDivElement>) =>{
+    if (textareaFocused) {
+      return;
+    }
     let startX = e.pageX;
     let startY = e.pageY;
   
@@ -88,6 +92,10 @@ export default function EditBox() {
   top -= 2;
   left -= 2;
 
+  const transform = `rotate(${
+    size === 1 ? selectedCmp.style.transform : 0
+  }deg)`;
+
   return (
     <>
     {size === 1 && <AlignLines canvasStyle={canvasStyle} />}
@@ -99,6 +107,7 @@ export default function EditBox() {
         left,
         width,
         height,
+        transform
       }}
       onMouseDown={onMouseDownOfCmp}
       onClick={(e) =>{
@@ -141,6 +150,7 @@ export default function EditBox() {
         />}
 
         <StretchDots zoom={zoom} style={{width,height}}></StretchDots>
+        {size === 1 && <Rotate zoom = {zoom} selectedCmp={selectedCmp} />}
       </div>
       </>
   );
